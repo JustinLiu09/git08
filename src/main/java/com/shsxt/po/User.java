5f353dbd0266aa8e0490e52084496333e29c8eda
JustinLiu09 <lcq0615@163.COM>
2018-04-15 23:13:12 +0800

    git08测试提交

---
+++ src/main/java/com/shsxt/po/User.java
@@ -0,0 +1,28 @@
+package com.shsxt.po;
+
+import com.sun.scenario.effect.impl.prism.PrImage;
+
+/**
+ * Created by Justin on 2018/4/15.
+ */
+public class User {
+
+    private Integer Id;
+    private String userName;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
