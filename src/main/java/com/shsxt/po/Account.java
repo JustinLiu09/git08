b0d09ccdf9796115aecdae1d9e942c074d746575
JustinLiu09 <lcq0615@163.COM>
2018-04-15 23:48:04 +0800

    git08测试提交

---
+++ src/main/java/com/shsxt/po/Account.java
@@ -0,0 +1,26 @@
+package com.shsxt.po;
+
+/**
+ * Created by Justin on 2018/4/15.
+ */
+public class Account {
+
+    private Integer id;
+    private Integer userId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
