192a5dad860324a5818c48b5bf2c50122dcfc074
JustinLiu09 <lcq0615@163.COM>
2018-04-15 23:51:41 +0800

    git08测试提交

---
+++ src/main/java/com/shsxt/po/Item.java
@@ -0,0 +1,26 @@
+package com.shsxt.po;
+
+/**
+ * Created by Justin on 2018/4/15.
+ */
+public class Item {
+
+    private Integer id;
+    private Integer itemId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+}
